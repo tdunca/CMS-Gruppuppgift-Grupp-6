f9c7f17dc9525c8883bd9d9fedf2ac2711b73601
Ehnwall <dennis.ehnwall@gmail.com>
2024-05-20 11:03:00 +0200

    add reag, post & get data to firebase

--- src/features/customer/components/footer.tsx
+++ src/features/customer/components/footer.tsx
@@ -1,5 +1,32 @@
+import { db } from '../../../main';
+import { getDocs, collection, query, where } from 'firebase/firestore';
+import { useState } from 'react';
+
 function Footer() {
-  return <>FOOTER FÖR FAN!</>;
+  const [houses, setHouses] = useState([]);
+  const [search, setSearch] = useState('');
+
+  async function handleClick() {
+    const q = query(collection(db, 'hus'), where('name', '==', search));
+    const snapshot = await getDocs(q);
+    const houses = snapshot.docs.map((doc) => doc.data());
+    setHouses(houses);
+  }
+
+  return (
+    <>
+      <input
+        placeholder="Sök hus här!"
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button onClick={handleClick}>Sök hus för fan!</button>
+      {houses &&
+        houses.map((house, index) => <div key={index}>{house.name}</div>)}
+      FOOTER FÖR FAN!
+    </>
+  );
 }
 
 export default Footer;
