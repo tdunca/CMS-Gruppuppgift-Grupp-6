88281def8e8d653a66575ff778a3babcf83f249c
Ehnwall <dennis.ehnwall@gmail.com>
2024-05-23 14:16:41 +0200

    add header style & button

--- src/features/customer/components/header.tsx
+++ src/features/customer/components/header.tsx
@@ -3,19 +3,15 @@ import Style from './header.module.css';
 function Header() {
   return (
     <header className={Style.header}>
-      <div className={Style.container}>
-        <div className={Style.logo}>
-          <img
-            src="../src/Assests/logo.png"
-            alt="Svensk Fastighetsförmedling"
-          />
-        </div>
-        <div className={Style.buttonContainer}>
-          <button className={Style.button}>Sök Bostad</button>
-          <button className={Style.button}>Sök Mäklare</button>
-          <button className={Style.button}>Nyproduktion</button>
-          <button className={Style.button}>Fri Värdering</button>
-        </div>
+      <div className={Style.logo}>
+        <img
+          src="../src/Assests/headerLogo.png"
+          alt="Svensk Fastighetsförmedling"
+        />
+      </div>
+      <div className={Style.buttonContainer}>
+        <input className={Style.label} placeholder="Sök hus här!" type="text" />
+        <button className={Style.button}>Sök</button>
       </div>
     </header>
   );
