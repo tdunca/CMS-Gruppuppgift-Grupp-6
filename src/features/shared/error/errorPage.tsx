7c6d11843d30a8b0c0d5baeebf64a375531a6a43
tdunca <dunca.theresa@gmail.com>
2024-05-30 13:12:08 +0200

    Updated Errorpage, favicon, title

--- src/features/shared/error/errorPage.tsx
+++ src/features/shared/error/errorPage.tsx
@@ -2,16 +2,16 @@ import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 
 function ErrorPage() {
   const error = useRouteError();
-	
+
   if (isRouteErrorResponse(error)) {
     return (
       <>
-        ROUTE ERROR FÖR FAN! {error.status} {error.statusText}
+        ROUTE ERROR! {error.status} {error.statusText}
       </>
     );
   }
 
-  return <>OTHER ERROR FÖR FAN!</>;
+  return <>OTHER ERROR!</>;
 }
 
 export default ErrorPage;
