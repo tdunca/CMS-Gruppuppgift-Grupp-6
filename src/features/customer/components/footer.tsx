3c800f0d1b248ab977788540b8acd0307fb4ec33
Linus Almgren <linus.almgren@gmail.com>
2024-05-30 12:37:14 +0200

    add footer styles

--- src/features/customer/components/footer.tsx
+++ src/features/customer/components/footer.tsx
@@ -1,4 +1,3 @@
-import facebooklogo from '../assets/facebooklogo.png';
 import instagramlogo from '../assets/instagramlogo.png';
 import linkedinlogo from '../assets/linkedinlogo.png';
 import twitterlogo from '../assets/twitterlogo.png';
@@ -7,9 +6,9 @@ import Style from './footer.module.css';
 function Footer() {
   return (
     <footer className={Style.footer}>
-      <section className={Style.footerSection}>
-        <p>0138 333 333 | info@elitehomes.com</p>
-        <p>Mäklargatan 23 | Mäklarberg | Sverige </p>
+      <p>0138 333 333 | info@elitehomes.com</p>
+      <p>Mäklargatan 23 | Mäklarberg | Sverige </p>
+      <div className={Style.socials}>
         <a className={Style.socialslink} href="https://www.instagram.com/">
           <img
             className={Style.socialspic}
@@ -17,17 +16,14 @@ function Footer() {
             alt="instagram"
           />
         </a>
-        <a className={Style.socialslink} href="https://www.facebook.com/">
-          <img className={Style.socialspic} src={facebooklogo} alt="facebook" />
-        </a>
         <a className={Style.socialslink} href="https://www.twitter.com/">
           <img className={Style.socialspic} src={twitterlogo} alt="twitter" />
         </a>
         <a className={Style.socialslink} href="https://www.linkedin.com/">
           <img className={Style.socialspic} src={linkedinlogo} alt="linkedin" />
         </a>
-        <p>© Elite Homes 2024</p>
-      </section>
+      </div>
+      <p>© Elite Homes 2024</p>
     </footer>
   );
 }
